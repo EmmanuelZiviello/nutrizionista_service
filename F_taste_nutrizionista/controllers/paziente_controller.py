ba196eed3fd6107b02b7b3add4fef5a79d2ddb03
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-11 16:14:54 +0100

    kafka a paziente che va a richieste

--- F_taste_nutrizionista/controllers/paziente_controller.py
+++ F_taste_nutrizionista/controllers/paziente_controller.py
@@ -39,6 +39,7 @@ class DiseaseDelPazienteController(Resource):
         return PazienteService.get_conditions(request_args["id_paziente"], email_nutrizionista)
 
 class Paziente(Resource):
+    #da spostare in nutrizionista controller /pazienti
     @nutrizionista_required()
     @nutrizionista_ns.expect(get_paziente, validate=True)
     @nutrizionista_ns.doc('aggiungi paziente al nutrizionista')
